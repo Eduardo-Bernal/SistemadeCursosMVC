d480cb47bf5392945b35796b65cbea468e3a257b
Eduardo-Bernal <cbedudu@gmail.com>
2025-11-11 20:09:00 -0300

    MVC Completo

--- obj/Debug/net9.0/SistemaDeCursoMVC.AssemblyInfo.cs
+++ obj/Debug/net9.0/SistemaDeCursoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaDeCursoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e48d999002dcff6dbe8e0a77e72512df0fcbc43a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaDeCursoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaDeCursoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
